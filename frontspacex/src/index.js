3d01a438833733a7cf04a0479723ac8a421c502d
Matthieu Tourneur <45032156+MattTour@users.noreply.github.com>
2022-10-04 17:16:40 +0200

    Dev (#5)
    * React template
    
    * Tec Launches (#2)
    
    * Développement de la fonctionnalité d'affichage des Launchs de fusées.
    
    Necessite les install de :
    npm install -S react-router-dom
    npm install --save bootstrap
    npm install axios
    
    * Fix implementation Bootstrap
    
    * Fonctionnalite appel API capsules et Affichage. (#3)
    
    Appel de l'API
    Affichage sur une page des informations
    Fix d'un argument inutile pour l'appel API des lancements de fusees.
    
    * Add homepage
    
    Co-authored-by: Pepito94510 <kevin.boissiere94@gmail.com>

---
+++ frontspacex/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
+import { Route, Routes } from 'react-router';
+
+// import Button from './componant/Button';
+import Launches from './pages/launches';
+import Home from './pages/home';
+import Capsules from './pages/capsules';
+
+import './index.css';
+
+const App = () => {
+
+    return <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/launches/" element={<Launches/>}></Route>
+        <Route path="/capsules/" element={<api/>}></Route>
+      </Routes>
+    </Router>
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
